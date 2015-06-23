a44b2f8e9939c180f41f4bb1a32644297b6b93d5
node-rookie <749924911@qq.com>
2015-06-23 15:14:30 +0800

    add log module

--- src/app/application.js
+++ src/app/application.js
@@ -1,48 +1,16 @@
 var koa = require('koa');
 var app = module.exports = koa();
-var logger = require('koa-logger');
 var views = require('co-views');
+var logging = require('./logging');
 var path = require('path');
-var staticServer = require('koa-static');
 
 
 var render= views(path.join(__dirname, '../views'), { map: { html: 'swig' }});
 
-app.use(logger());
+app.use(logging.generatorFunc);
 //router
 require('../routes')(app);
 
-app.use(function *() {
-    switch (this.path) {
-        case '/get':
-            get.call(this);
-            break;
-        case '/remove':
-            remove.call(this);
-            break;
-        case '/regenerate':
-            yield regenerate.call(this);
-            break;
-    }
-});
-
-function get() {
-    var session = this.session;
-    session.count = session.count || 0;
-    session.count++;
-    this.body = session.count;
-}
-
-function remove() {
-    this.session = null;
-    this.body = 0;
-}
-
-function *regenerate() {
-    get.call(this);
-    yield this.regenerateSession();
-    get.call(this);
-}
 //404
 app.use(function *pageNotFound(next) {
     this.body = yield render('404');
